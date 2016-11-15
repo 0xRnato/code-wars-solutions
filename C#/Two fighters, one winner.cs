ab14d4a0717997813cbb6a5aba821f699f30ba4d
Renato Neto <rnato.netoo@gmail.com>
2016-11-15 05:10:57 -0200

    Two fighters, one winner - 7 kyu

--- C#/Two fighters, one winner.cs
+++ C#/Two fighters, one winner.cs
@@ -1,3 +1,35 @@
+// Description:
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// public class Fighter {
+//   public string Name;
+//   public int Health, DamagePerAttack;
+//   public Fighter(string name, int health, int damagePerAttack) {
+//     this.Name = name;
+//     this.Health = health;
+//     this.DamagePerAttack = damagePerAttack;
+//   }
+// }
+// Example:
+
+//   declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+//   // Python
+//   declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+
+//   Lew attacks Harry; Harry now has 3 health.
+//   Harry attacks Lew; Lew now has 6 health.
+//   Lew attacks Harry; Harry now has 1 health.
+//   Harry attacks Lew; Lew now has 2 health.
+//   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+
 public class Kata {
   public static string declareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker) {
     // Your code goes here. Have fun!
